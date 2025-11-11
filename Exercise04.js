8db1d9aba1ddf0bbedbccab833b7a508a9293b3a
Ashanshi <ashanshins99@gmail.com>
2025-11-11 21:57:10 +0200

    Module01

---
+++ Exercise04.js
@@ -0,0 +1,26 @@
+const studentname = prompt( 'Enter Your Good Name, Young Wizard:');
+const randomnumber = Math.floor(Math.random() *4 + 1);
+
+let house
+
+if
+(randomnumber == 1){
+house = "Gryffindor";
+}
+
+else if
+(randomnumber == 2){
+house = "Slytherin";
+}
+
+else if
+(randomnumber == 3){
+house = "Hufflepuff";
+}
+
+else{
+house = "Ravenclaw";
+}
+
+document.querySelector('#result').innerHTML = `
+  ${studentname}, You are ${house}, !`
